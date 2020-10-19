168d183140d487b6daabbd08ea2e82b17cc0af03
sergeyelfimov1 <sergey.elfimov@fdiba.tu-sofia.bg>
2020-10-19 19:56:22 +0000

    1

--- src/exercise3/NumberSearch.java
+++ src/exercise3/NumberSearch.java
@@ -3,39 +3,21 @@ package exercise3;
 import java.util.Scanner;
 
 public class NumberSearch {
-<<<<<<< HEAD
-
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int count =0;
-        for(int i=100;i<=1000;i++){
-            
-          if(i%5==0 && i%6==0 ){
-            count++;
-            if(count !=10){
-              System.out.print(i +" ");
-            }
-            else{
-              System.out.println(i);
-              count =0;
-            }
-          }  
-          
-        }
-    }
-
-}
-=======
-  public static void main(String[] args)  {
-    Scanner input = new Scanner(System.in);
+  public static void main(String[] args) {
     int count = 0;
-    for (int number = 100; int number <=1000; number++) {
-      if (number % 30 == 0) {
+    for (int number = 100; number <= 1000; number++) {
+      if(number % 30 == 0) {
         count++;
+      if (count==10) { 
+        count = 0;
+        System.out.print(number);
+        System.out.println();
+      } else {
+        System.out.print(number + " ");
       }
-    }
-
-
+      }
+      
+    
   }
 }
->>>>>>> origin/master
+}
--- src/exercise3/PrimeNumber.java
+++ src/exercise3/PrimeNumber.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class PrimeNumber {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-<<<<<<< HEAD
     int number = input.nextInt();
     int d = 2;
     boolean prime = true;
@@ -19,21 +18,3 @@ public class PrimeNumber {
     System.out.println("Prime number: " + prime);
   }
 }
-=======
-    int a = input.nextInt();
-    int b = 2;
-    boolean prime = true;
-    while (a > 2) {
-      if (a % b == 0) {
-        prime = false;
-        System.out.println("Prime number: " + prime);
-      } else {
-        prime = true;
-        System.out.println("Prime number: " + prime);
-      }
-      break;
-
-    }
-  }
-}
->>>>>>> origin/master
